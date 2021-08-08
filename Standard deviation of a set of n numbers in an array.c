7a11269c7e25e5a63683630cb04037a715116f53
Preethi Samantha Bennet <82455813+PreethiSamanthaBennet@users.noreply.github.com>
2021-08-08 17:58:22 +0530

    Create Standard deviation of a set of n numbers in an array.c

---
+++ Standard deviation of a set of n numbers in an array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(/*float data[]*/)
+{
+	int i,n=0;
+	float sum=0.0,mean,sd=0.0;
+	
+	scanf("%d",&n);
+	
+	float data[n];
+	
+	for(i=0; i < n; i++){
+		scanf("%f", &data[i]);
+	}
+		
+	for(i=0; i<n; ++i){
+		sum += data[i];
+		mean = sum/n;
+	}
+	
+	for(i=0; i<n; ++i){
+		sd += pow((data[i] - mean), 2);
+	}
+		 
+	printf("Standard Deviation = %.6f", sd/n);   
+	
+	return 0;
+}
